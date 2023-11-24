329dd52fcfbdb0db7eac92d2deda6910c7087ad8
Alexandr <babiyksasha21@gmail.com>
2023-11-24 22:48:36 +0200

    added a script for the year and month buttons to switch prices in blocks with tariffs for monthly payment and annual payment

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {});
+const btnYear = document.querySelector('.rates__header-btn-year');
+const btnMonth = document.querySelector('.rates__header-btn-right');
+const priceMonth = document.querySelectorAll('.rates__price-month');
+const priceYear = document.querySelectorAll('.rates__price-year');
+
+btnMonth.addEventListener('click', () => {
+  priceMonth.forEach((month) => {
+    month.style.display = 'block';
+  });
+
+  btnMonth.classList.add('rates__header-btn-month');
+  btnYear.classList.remove('rates__header-btn-year');
+  priceYear.forEach((year) => {
+    year.style.display = 'none';
+  });
+});
+
+btnYear.addEventListener('click', () => {
+  priceYear.forEach((year) => {
+    year.style.display = 'block';
+  });
+
+  btnYear.classList.add('rates__header-btn-year');
+  btnMonth.classList.remove('rates__header-btn-month');
+  priceMonth.forEach((month) => {
+    month.style.display = 'none';
+  });
+});
